c13a5545dee217c14d34c0851914f65696eca831
niloyKGhosh <niloykumar256@gmail.com>
2020-05-02 10:54:39 -0400

    connected database and models with app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-require;
+// Loading all the dependencies
+const express = require("express");
+const app = express();
+require("./db/mongoose");
+
+// Loading all the models
+const User = require("./models/users");
+
+const user = new User({
+	name: "Niloy",
+	email: "niloykumar256@gmail.com",
+	password: "hulalala3456",
+	age: "29",
+});
