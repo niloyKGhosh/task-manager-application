784f157e95e46a9c14f7686f3ce8e3d032f42edd
niloyKGhosh <niloykumar256@gmail.com>
2020-05-02 10:20:39 -0400

    Created the user schema

---
+++ src/db/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const dbUrl = "mongodb://127.0.0.1:27017/task-manager-api";
+
+mongoose.connect(dbUrl, {
+	useCreateIndex: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
