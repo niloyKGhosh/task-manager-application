fdee7a012122ad370ca6b335ca9f546f61659e86
niloyKGhosh <niloykumar256@gmail.com>
2020-05-02 10:28:23 -0400

    Added password and age to the user schema

--- src/models/users.js
+++ src/models/users.js
@@ -18,4 +18,26 @@ const User = mongoose.model("User", {
 			}
 		},
 	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 7,
+		trim: true,
+		validate(value) {
+			if (value.toLowerCase().includes("password")) {
+				throw new Error("Password cannot contain 'password'");
+			}
+		},
+	},
+	age: {
+		type: Number,
+		default: 0,
+		validate(value) {
+			if (value < 0) {
+				throw new Error("Age must be a positive number");
+			}
+		},
+	},
 });
+
+module.exports = User;
