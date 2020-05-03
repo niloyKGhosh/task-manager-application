027c9e26d175c797a2b3e870e0bc365639e38f53
niloyKGhosh <niloykumar256@gmail.com>
2020-05-03 18:21:54 -0400

    Refactored code using promise syntax

--- src/app.js
+++ src/app.js
@@ -13,15 +13,15 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Endpoint to create the users list in the database.
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
 	const user = new User(req.body);
-	user.save()
-		.then(() => {
-			res.status(201).send(user);
-		})
-		.catch((error) => {
-			res.status(400).send(error);
-		});
+
+	try {
+		await user.save();
+		res.status(201).send(user);
+	} catch (error) {
+		res.status(400).send(error);
+	}
 });
 
 // Endpoint to create the tasks list in the database.
@@ -37,30 +37,28 @@ app.post("/tasks", (req, res) => {
 		});
 });
 
-app.get("/users", (req, res) => {
-	User.find({})
-		.then((users) => {
-			res.send(users);
-		})
-		.catch((error) => {
-			res.status(500).send();
-		});
+// Get all the users stored in the database.
+app.get("/users", async (req, res) => {
+	try {
+		const users = await User.find({});
+		res.send(users);
+	} catch (error) {
+		res.status(500).send();
+	}
 });
 
 // Get a specific user by a specific id by route parameters indicated by :id
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
 	const _id = req.params.id;
-
-	User.findById(_id)
-		.then((user) => {
-			if (!user) {
-				return res.status(404).send("No users found!");
-			}
-			res.send(user);
-		})
-		.catch((error) => {
-			res.status(500).send();
-		});
+	try {
+		const user = await User.findById(_id);
+		if (!user) {
+			return res.status(404).send("No users found!");
+		}
+		res.send(user);
+	} catch (e) {
+		res.status(500).send();
+	}
 });
 
 // The app starts the server and listens to port for incoming requests to routes.
