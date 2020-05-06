ad0f8087c61dde12b447b011edd4b7a710378093
niloyKGhosh <niloykumar256@gmail.com>
2020-05-06 01:15:00 -0400

    Created the structure for user login

--- src/models/users.js
+++ src/models/users.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		required: true,
+		unique: true, // doesn't save accounts with same email
 		trim: true,
 		lowercase: true,
 		validate(value) {
@@ -39,6 +40,23 @@ const userSchema = new mongoose.Schema({
 		},
 	},
 });
+
+userSchema.statics.findByCredentials = async (email, password) => {
+	const user = await User.findOne({ email });
+
+	if (!user) {
+		throw new Error("Unable to login");
+	}
+
+	const isMatch = await bcrypt.compare(password, user.password);
+	console.log(isMatch);
+	if (!isMatch) {
+		console.log("I am here");
+		throw new Error("Unable to login");
+	}
+
+	return user;
+};
 
 userSchema.pre("save", async function (next) {
 	// this refers to the user that is being saved
