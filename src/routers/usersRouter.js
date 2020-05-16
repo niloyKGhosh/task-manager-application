0f45dae96025ef39c723dd6c0deeea1b091238d9
niloyKGhosh <niloykumar256@gmail.com>
2020-05-16 03:42:51 -0400

    Added profile photo upload

--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -91,10 +91,35 @@ router.delete("/users/me", auth, async (req, res) => {
 	}
 });
 const upload = multer({
-	dest: "avatars",
+	limits: {
+		fileSize: 1000000,
+	},
+	fileFilter(req, file, cb) {
+		if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+			return cb(new Error("Please upload an image!"));
+		}
+		cb(undefined, true);
+	},
 });
-router.post("/users/me/avatar", upload.single("avatar"), (req, res) => {
-	res.send("Ok");
-});
+router.post(
+	"/users/me/avatar",
+	auth,
+	upload.single("avatar"),
+	async (req, res) => {
+		// req.file.buffer contains the picture
+		req.user.avatar = req.file.buffer;
 
+		await req.user.save();
+		res.send({ success: "Image Upload Successful" });
+	},
+	(error, req, res, next) => {
+		res.status(400).send({ error: error.message });
+	}
+);
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+	req.user.avatar = undefined;
+	await req.user.save();
+	res.send({ message: "Profile Photo deleted successfully" });
+});
 module.exports = router;
